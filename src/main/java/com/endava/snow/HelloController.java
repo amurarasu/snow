83f7a8e5dc7e9c62d576ecbdbec0803a7666eed2
Alexandru Murarasu <alexandru.murarasu@endava.com>
2018-12-25 15:57:58 +0200

    add hello controller

---
+++ src/main/java/com/endava/snow/HelloController.java
@@ -0,0 +1,14 @@
+package com.endava.snow;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class HelloController {
+
+  @GetMapping
+  public String helloSnow() {
+    return "Hello Snow!";
+  }
+
+}
